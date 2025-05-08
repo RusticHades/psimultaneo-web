510e2a7c0eaedd5bb0f269d40b7bb7a6d73bd1d6
RusticHades1578 <christiangarduno292@gmail.com>
2025-05-07 18:57:19 -0600

    Configuración corregida para producción

--- create_superuser.py
+++ create_superuser.py
@@ -1,7 +1,15 @@
+# create_superuser.py
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psimultaneo.settings')
 import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psimultaneo.settings')
 django.setup()
+
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
-User.objects.create_superuser('admin', 'psimultaneo.web@gmail.com', 'GMCA8900g')
+if not User.objects.filter(username='PsimultaneoAdmin').exists():
+    User.objects.create_superuser('PsimultaneoAdmin', 'psimultaneo.web@gmail.com', 'GMCA8900g')
+    print("Superusuario creado!")
+else:
+    print("El superusuario ya existe.")
